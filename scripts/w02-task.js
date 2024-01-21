331ead644b8e8d338a0fcb4bbd7a86cf92a3b4fe
Sawyer Crawley <crawleysawyer@gmail.com>
2024-01-20 22:09:25 -0700

    Completed W02 tasks

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,43 +3,26 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = 'Sawyer Crawley';
-let currentYear = '2024';
-let profilePicture = 'images/me.jpg';
+const fullName = 'Sawyer Crawley';
+const currentYear = new Date().getFullYear();
+const profilePicture = 'images/me.jpg';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = profilePicture;
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
-imageElement.setAttribute('src', 'profilePicture');
+imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-let favoriteFoods = ['Sushi', 'Mangoes', 'Everything Bagels', 'Steak', 'Shrimp']
-favoriteFoods.forEach(food => {
-    const foodItem = document.createElement('div');
-    foodItem.textContent = food;
-    foodElement.appendChild(foodItem);
-  });
-let newFavoriteFood = 'BLTs';
-favoriteFoods.push(newFavoriteFood);
+let favoriteFoods = ["Burritos", "Steak", "Terriyaki Chicken", "Pistachio Icecream", "Buffalo Wings", "Root Beer"];
+
+foodElement.innerHTML = favoriteFoods.join;
+let newFood = 'Pineapple';
+favoriteFoods.push(newFood);
 foodElement.innerHTML += `<br>${favoriteFoods}`;
-favoriteFoods.shift();
-favoriteFoods.forEach(food => {
-    const foodItem = document.createElement('div');
-    foodItem.textContent = food;
-    foodElement.appendChild(foodItem);
-    foodElement.innerHTML += '<br>';
-  });
-favoriteFoods.pop();
-favoriteFoods.forEach(food => {
-    const foodItem = document.createElement('div');
-    foodItem.textContent = food;
-    foodElement.appendChild(foodItem);
-    foodElement.innerHTML += '<br>';
-  });
